697b5afedb4fa5e113477b63ecd20c7d4a1b3c74
jackchi <fuching.chi@gmail.com>
2013-03-18 11:20:08 -0700

    First Commit: Added all working files for submission

---
+++ index.php
@@ -0,0 +1,146 @@
+<HTML>
+	<HEAD>
+		<title>:::: Nitevibe :::: Event Submission Guidelines</title> 
+		<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+
+	</HEAD>
+<body bgcolor="#000000" link="#ffff66" vlink="#ffff66" alink="#ffff66" leftmargin="6"
+		topmargin="0" marginwidth="0" marginheight="0">
+		<table width="347" border="0" align="center" cellpadding="10" cellspacing="3">
+			<tr>
+				<td width="321" align="left" valign="top" bgcolor="#666666"><p><a href="http://www.nitevibe.com" target="_blank"><img src="http://www.nitevibe.com/gallery/images/nitevibe_logo.gif" width="187" height="34"
+								border="0"></a><br>
+        <em><strong><font color="#99ccff" size="4" face="Arial, Helvetica, sans-serif">Submit 
+        Your Event Here!</font></strong></em><br>
+					</p>
+					
+				</td>
+			</tr>
+			<tr>
+				<td height="483" valign="top" bgcolor="#666666">
+					<blockquote>
+						<form name="Event" method="post" enctype="multipart/form-data" action="email.php">
+          <p><font color="#FFFFFF" size="2" face="Verdana, Arial, Helvetica, sans-serif"><strong><font face="Arial, Helvetica, sans-serif"><font color="#FFFFCC" size="1">SELECTION 
+            PROCESS:</font></font></strong> </font><font color="#FFFFFF" size="2" face="Verdana, Arial, Helvetica, sans-serif"><br>
+            <font size="1">Events we cover range from VIP to Underground happenings, 
+            Nitevibe also cover DJ-centric events, benefits, art and fashion shows, 
+            boat party, street parties and anywhere the dance groove lives. </font></font><font color="#FFFFFF" size="2" face="Verdana, Arial, Helvetica, sans-serif"><font size="1"> 
+            A great majority of our events are 21 and over, and take place in 
+            San Francisco.</font></font></p>
+          <p><font color="#FFFFFF" size="2" face="Verdana, Arial, Helvetica, sans-serif"><strong><font face="Arial, Helvetica, sans-serif"><font color="#FFFFCC" size="1">EVENT 
+            SUBMISSION DEADLINE:</font></font></strong><br>
+            <font size="1">The BUZZ is produced one week in advance, and is published 
+            the following Tuesday for all events relating to the current week: 
+            Tuesday-Sunday. To all promoters and club owners - for your event 
+            to be considered for the BUZZ, you must submit your event no later 
+            then Tuesday prior to the week that your event takes place.</font></font></p>
+      <table width="259" border="0" cellpadding="1" cellspacing="0">
+            <tr> 
+              <td align="right" valign="top"><font color="#ffffff" size="1" face="Arial, Helvetica, sans-serif"><strong><font color="#FFCC00">Promoter&nbsp;Name: 
+                </font></strong> </font></td>
+              <td><font color="#ffffff" size="1" face="Verdana, Arial, Helvetica, sans-serif"> 
+                <input type="text" name="promoter_name" id="teield3">
+                </font></td>
+            </tr>
+            <tr> 
+              <td align="right" valign="top"><font color="#ffffff" size="1" face="Arial, Helvetica, sans-serif"><strong><font color="#ffffff" size="1" face="Arial, Helvetica, sans-serif"><strong><font color="#FFCC00">Promoter</font></strong></font><font color="#FFCC00"> 
+                Email:</font></strong></font></td>
+              <td><font color="#ffffff" size="1" face="Verdana, Arial, Helvetica, sans-serif"> 
+                <input type="text" name="promoter_email" id="txtDoorCo">
+                <br>
+                <img src="http://www.nitevibe.com/buzz/Images/clear.gif" width="13" height="10"> 
+                </font></td>
+            </tr>
+             <tr> 
+              <td width="89" align="right"> <div align="right"> 
+                  <p><font color="#FFCC33" size="1" face="Arial, Helvetica, sans-serif"><strong>Event 
+                    Day:</strong></font></p>
+                </div></td>
+              <td width="166"><select name="event_day" id="select">
+                  <option value="" selected>-- Pick Day --</option>
+                  <option>Monday</option>
+                  <option>Tuesday</option>
+                  <option>Wednesday</option>
+                  <option>Thursday</option>
+                  <option>Friday</option>
+                  <option>Saturday</option>
+                  <option>Sunday</option>
+                </select></td>
+            </tr>
+            <tr> 
+              <td align="right"><font color="#FFCC00" size="1" face="Arial, Helvetica, sans-serif"><strong>Event 
+                Date:</strong></font> </td>
+              <td><input name="event_date" type="text" size="10" id="txtEventDate3"></td>
+            </tr>
+            <tr> 
+              <td align="right"><font color="#FFCC00" size="1" face="Arial, Helvetica, sans-serif"><strong>Event 
+                Name:</strong> </font> </td>
+              <td><input type="text" name="event_name" id="txtEventName2"></td>
+            </tr>
+            <tr> 
+              <td align="right"><font color="#FFCC00" size="1" face="Arial, Helvetica, sans-serif"><strong>Venue:</strong> 
+                </font> </td>
+              <td><input type="text" name="event_venue" id="txtEventVenue2"></td>
+            </tr>
+            <tr> 
+              <td align="right"><font color="#FFCC00" size="1" face="Arial, Helvetica, sans-serif"><strong>Artists:</strong> </font></td>
+              <td><textarea name="event_artists" cols="20" rows="3" id="textarea"></textarea></td>
+            </tr>
+            <tr> 
+              <td align="right"><font color="#ffffff" size="1" face="Arial, Helvetica, sans-serif"><strong><font color="#FFCC00">Musical 
+                Style: </font></strong> </font> <font color="#FFCC00" size="1" face="Verdana, Arial, Helvetica, sans-serif">&nbsp; 
+                </font></td>
+              <td><font color="#ffffff" size="1" face="Verdana, Arial, Helvetica, sans-serif"> 
+                <input type="text" name="event_style" id="textfield3">
+                </font></td>
+            </tr>
+            <tr> 
+              <td align="right"><font color="#ffffff" size="1" face="Arial, Helvetica, sans-serif"><strong><font color="#FFCC00">Door 
+                Cover:</font></strong></font><font color="#ffffff" size="1" face="Verdana, Arial, Helvetica, sans-serif">&nbsp;</font></td>
+              <td><font color="#ffffff" size="1" face="Verdana, Arial, Helvetica, sans-serif"> 
+                <input type="text" name="event_cover" id="txtDoorCover3">
+                </font></td>
+            </tr>
+            <tr> 
+              <td align="right"><font color="#ffffff" size="1" face="Arial, Helvetica, sans-serif"><strong><font color="#FFFFCC">Promoter 
+                Link:</font></strong></font> </td>
+              <td><font color="#ffffff" size="1" face="Verdana, Arial, Helvetica, sans-serif"> 
+                <input type="text" name="event_link" id="txtDoorCover3">
+                </font></td>
+            </tr>
+            <tr> 
+              <td height="116" align="right"><div align="right"><font color="#ffffff" size="1" face="Arial, Helvetica, sans-serif"><font color="#FFFFCC">Suggested 
+                  Description<br>
+                  (100 Words Max)</font></font> </div></td>
+              <td><font color="#ffffff" size="1" face="Verdana, Arial, Helvetica, sans-serif"> 
+                <textarea name="event_description" cols="20" rows="6" id="textarea2"></textarea>
+                </font></td>
+            </tr>
+            
+            <tr> 
+              <td colspan="2"><font color="#ffffff" size="1" face="Arial, Helvetica, sans-serif"><strong><font color="#FFFFCC" size="2">Upload 
+                Flyer Image</font><font color="#FFFFCC"><br>
+                (Front Flyer Image)(5Mb Max)</font></strong>
+                </td>
+            </tr>
+            <tr valign="top"> 
+              <td colspan="2">
+                  <input name="img1" type="file" size="500px" id="txtEventDate22">
+                  <br />   
+                 
+                 
+            </tr>
+                     </table>
+          
+           <?php
+          require_once('recaptchalib.php');
+          $publickey = "6LdjR9kSAAAAAHwFeikS5Q2NqIiRSLBHi3Eyw8d6"; // you got this from the signup page
+          echo recaptcha_get_html($publickey);
+        ?>         
+          <p align="center"><br />
+            
+            <input type="submit" name="Submit" value="Submit" id="btnSubmit2">
+          </p>
+          
+          
+          </form> 
